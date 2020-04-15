d18991f08f1f0dda35dfcc2f3efd7dfbf9f06396
Juliana Ariffin <juliana@fastmail.com>
2020-04-15 16:00:33 +0200

    working wikinimous

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
+// require jquery
+// require bootstrap-sprockets
+// require jquery_ujs
+// require turbolinks
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
